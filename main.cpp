86b440dff12344b72219f6c42434ba254d02d553
Artanidos <artanidos@gmail.com>
2023-01-14 05:54:50 +0100

    upd

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickWindow>
 #include <QDebug>
 #include "seo.h"
+
+QQmlApplicationEngine *eng;
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QStringList args = app.arguments();
-    QString hash = args.at(args.length()-1);
-    qDebug() << hash;
+    QQmlApplicationEngine engine;
+    eng = &engine;
 
     qmlRegisterType<Seo>("at.crowdware.wasm", 1, 0, "Seo");
 
-    QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/qmlwasm/main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject *obj, const QUrl &objUrl) 
     {
@@ -23,3 +24,15 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+extern "C" {
+    void hashchanged(char *hash)
+    {
+        QString url = "https://raw.githubusercontent.com/CrowdWare/QmlWasm/web/content/" + QString::fromUtf8(hash) + ".qml";
+        qDebug() << url << "will be loaded";
+
+        QObject *win = eng->rootObjects().constFirst();
+        QMetaObject::invokeMethod(win, "loadPage", Q_ARG(QString, url));
+    }
+}
+
--- main.qml
+++ main.qml
@@ -5,15 +5,24 @@ import QtQuick.Layouts 1.3
 
 Window {
     visible: true
-    width: 640
-    height: 480
     color: "#171C2C"
     title: "Dynamic QML"
 
     StackView {
         id: stackview
-        initialItem: "https://raw.githubusercontent.com/CrowdWare/QmlWasm/web/content/app.qml"
+        initialItem: text
         anchors.centerIn: parent
+    }
 
+    Text {
+        id: text
+        anchors.centerIn: parent
+        color: "white"
+        text: "Hello world"
+    }
+
+    function loadPage(url: string)
+    {
+        stackview.replace(url)
     }
 }
